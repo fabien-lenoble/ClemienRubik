4d6189ac9d2e6040cb0d96a83aa6c30108da3d74
cvanehcke <clement.van-hecke@netatmo.com>
2023-02-08 14:27:16 +0100

    fix base url for dev

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "/ClemienRubik/",
+  base: process.env.NODE_ENV === "production" ? "/ClemienRubik/" : "/",
 });
