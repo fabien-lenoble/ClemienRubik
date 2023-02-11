4e7d0d2926f133a9c06843d16fdbbc5b1165d4f6
flenoble <fabien.lenoble@legrand.com>
2023-02-11 16:16:10 +0100

    add last averages logic

--- src/components/CurrentScramble/LastAverages/index.vue
+++ src/components/CurrentScramble/LastAverages/index.vue
@@ -1,25 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useSession } from "@/composables/session";
+
+const { lastAo5, lastAo12, mean, getPersonalBest } = useSession();
+</script>
 
 <template>
   <div class="flex flex-col h-full">
     <div class="flex h-full">
       <div class="basis-full self-center">
         <div class="text-xs">Ao5</div>
-        <div class="">1:12.039</div>
+        <div class="">{{ lastAo5 }}</div>
       </div>
       <div class="basis-full self-center">
         <div class="text-xs">Ao12</div>
-        <div class="">1:20.234</div>
+        <div class="">{{ lastAo12 }}</div>
       </div>
     </div>
     <div class="flex h-full">
       <div class="basis-full self-center">
-        <div class="text-xs">Ao100</div>
-        <div class="">1:22.988</div>
+        <div class="text-xs">PB</div>
+        <div class="">{{ getPersonalBest.displayTime }}</div>
       </div>
       <div class="basis-full self-center">
         <div class="text-xs">Mean</div>
-        <div class="">1:21.030</div>
+        <div class="">{{ mean }}</div>
       </div>
     </div>
   </div>
