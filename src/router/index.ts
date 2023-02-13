e90276ed768588ae2ed42abc111233c74cbdef5e
fabienlenoble <33342346+fabienlenoble@users.noreply.github.com>
2023-02-13 21:25:41 +0100

    add auto redirect to scramble page if we go to /

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import ScrambleView from "@/views/ScrambleView.vue";
+import { useSession } from "@/composables/session";
+import { useScramble } from "@/composables/scramble";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/",
+      redirect: redirectToScramblePage(),
+    },
     {
       path: "/scramble/:seed/:scrambleIndex",
       name: "scramble",
@@ -11,5 +17,18 @@ const router = createRouter({
     },
   ],
 });
+
+function redirectToScramblePage() {
+  const { sessionSolves } = useSession();
+  const { currentScrambleIndex } = useScramble();
+  const firstSessionSolve = sessionSolves.value[0];
+  let seed = Math.floor(Math.random() * 10000000000000);
+  let scrambleIndex = 1;
+  if (firstSessionSolve) {
+    seed = firstSessionSolve.seed;
+    scrambleIndex = currentScrambleIndex.value;
+  }
+  return `/scramble/${seed}/${scrambleIndex}`;
+}
 
 export default router;
