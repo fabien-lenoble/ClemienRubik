7c571690581e02ba566bc206a276af2d791be5dc
fabienlenoble <33342346+fabienlenoble@users.noreply.github.com>
2023-04-19 21:40:30 +0200

    share session: add possibility to open camera to scan the qr code

--- src/components/CurrentScramble/ScrambleInfo/SharePopup.vue
+++ src/components/CurrentScramble/ScrambleInfo/SharePopup.vue
@@ -13,6 +13,15 @@ function copyUrl() {
   navigator.clipboard.writeText(window.location.href);
   copyLinkClicked.value = true;
 }
+
+async function getMedia() {
+  try {
+    return await navigator.mediaDevices.getUserMedia();
+    /* use the stream */
+  } catch (err) {
+    /* handle the error */
+  }
+}
 </script>
 
 <template>
@@ -52,6 +61,15 @@ function copyUrl() {
               @click="copyUrl"
             >
               {{ copyLinkClicked ? "Copied" : "Copy link" }}
+            </button>
+          </div>
+          <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+            <button
+              type="button"
+              class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              @click="getMedia()"
+            >
+              Join session
             </button>
           </div>
           <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
