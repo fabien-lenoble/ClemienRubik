2c500caa1b6d2bb185ab1a06009feacd02c4a65a
fabienlenoble <33342346+fabienlenoble@users.noreply.github.com>
2023-05-03 19:35:54 +0200

    join/share: fix shared url only being evaluated once at the launch of the app

--- src/composables/share/index.ts
+++ src/composables/share/index.ts
@@ -1,10 +1,12 @@
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import type { Ref } from "vue";
+import { useRoute } from "vue-router";
 const isShareModalOpen: Ref<boolean> = ref(false);
 const isJoinModalOpen: Ref<boolean> = ref(false);
-const shareUrl: Ref<string> = ref(
-  window.location.href.replace("scramble", "join")
-);
+const shareUrl: Ref<string> = computed(() => {
+  const route = useRoute();
+  return route.fullPath.replace("scramble", "join");
+});
 export function useShare() {
   return {
     isShareModalOpen,
