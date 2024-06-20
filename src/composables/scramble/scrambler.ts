ace94f6008d341f299950885082744f14ea76328
Fabien Lenoble <fabien.lenoble1@gmail.com>
2024-06-20 22:02:40 +0200

    remove scrambow (not random enough & too slow)

--- src/composables/scramble/scrambler.ts
+++ src/composables/scramble/scrambler.ts
@@ -1,9 +1,8 @@
-import { ref } from "vue";
-import type { Ref } from "vue";
-import type { DisplayScramble, Scramble } from "./types";
-import { useRoute } from "vue-router";
 import { useSeed } from "@/composables/seed";
-import { Scrambow } from "scrambow";
+import type { Ref } from "vue";
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import type { DisplayScramble, MoveSet, Scramble, SubMoveSet } from "./types";
 
 const {
   baseSessionSeed,
@@ -17,6 +16,15 @@ const currentScrambleIndex: Ref<number> = ref(0);
 const numberOfMoves: Ref<number> = ref(20);
 const currentScramble: Ref<Scramble> = ref([]);
 const scramblesHistory: Ref<Scramble[]> = ref([]);
+
+const moveSet: MoveSet = [
+  ["U", "U'", "U2"],
+  ["D", "D'", "D2"],
+  ["F", "F'", "F2"],
+  ["B", "B'", "B2"],
+  ["L", "L'", "L2"],
+  ["R", "R'", "R2"],
+];
 
 function initScrambleValues() {
   const route = useRoute();
@@ -43,16 +51,106 @@ function goToNextScramble() {
 }
 
 function generateScramble() {
-  const scramble = new Scrambow()
-    .setSeed(Number(currentScrambleSeed.value))
-    .setLength(20)
-    .get(1)[0];
+  const scramble: Scramble = [];
+  let currentSubMoveSetIndex = -1;
+  let lastSubMoveSetIndex = -1;
+  let secondToLastSubMoveSetIndex = -1;
+  let currentSubMoveSet = null;
 
-  currentScramble.value = scramble.scramble_string
-    .trim()
-    .split(" ") as Scramble;
+  // iterate user requested "numberOfMove" times
+  for (let i = 0; i < numberOfMoves.value; i++) {
+    ({ currentSubMoveSetIndex, currentSubMoveSet } = pickSubMoveSetIndex(
+      currentSubMoveSetIndex,
+      lastSubMoveSetIndex,
+      secondToLastSubMoveSetIndex
+    ));
 
+    const move = pickMoveFromSubMoveSet(currentSubMoveSet);
+    scramble.push(move);
+
+    // store used sub move set indices to make sure we don't use them in next iterations
+    secondToLastSubMoveSetIndex = lastSubMoveSetIndex;
+    lastSubMoveSetIndex = currentSubMoveSetIndex;
+  }
+  currentScramble.value = scramble;
+}
+
+function pickSubMoveSetIndex(
+  currentSubMoveSetIndex: number,
+  lastSubMoveSetIndex: number,
+  secondToLastSubMoveSetIndex: number
+) {
+  // check if current sub move set is the same as the previous one
+  // (to avoid having the same letter 2+ consecutive times in the scramble
+  // OR 3 letters of the same 'opposite group' in a row, like F2 B F2 could actually
+  // be simplified as B for example because F and B, being opposite, do not alter each other's face)
+  while (
+    shouldRePickMoveSet(
+      currentSubMoveSetIndex,
+      lastSubMoveSetIndex,
+      secondToLastSubMoveSetIndex
+    )
+  ) {
+    // while it is, generate a new random seed and pick a new sub move set
+    generateNewSeed();
+    currentSubMoveSetIndex = lastGeneratedSeed.value % moveSet.length;
+  }
+  const currentSubMoveSet = moveSet[currentSubMoveSetIndex];
+  return { currentSubMoveSetIndex, currentSubMoveSet };
+}
+
+function shouldRePickMoveSet(
+  currentSubMoveSetIndex: number,
+  lastSubMoveSetIndex: number,
+  secondToLastSubMoveSetIndex: number
+) {
+  let areAllSubMoveSetsFromSameOppositeGroups = false;
+  if (
+    checkAreAllSubMoveSetsFromSameOppositeGroups(
+      [0, 1],
+      currentSubMoveSetIndex,
+      lastSubMoveSetIndex,
+      secondToLastSubMoveSetIndex
+    ) ||
+    checkAreAllSubMoveSetsFromSameOppositeGroups(
+      [2, 3],
+      currentSubMoveSetIndex,
+      lastSubMoveSetIndex,
+      secondToLastSubMoveSetIndex
+    ) ||
+    checkAreAllSubMoveSetsFromSameOppositeGroups(
+      [4, 5],
+      currentSubMoveSetIndex,
+      lastSubMoveSetIndex,
+      secondToLastSubMoveSetIndex
+    )
+  ) {
+    areAllSubMoveSetsFromSameOppositeGroups = true;
+  }
+  return (
+    currentSubMoveSetIndex === lastSubMoveSetIndex ||
+    areAllSubMoveSetsFromSameOppositeGroups
+  );
+}
+
+function checkAreAllSubMoveSetsFromSameOppositeGroups(
+  groups: number[],
+  currentSubMoveSetIndex: number,
+  lastSubMoveSetIndex: number,
+  secondToLastSubMoveSetIndex: number
+) {
+  return [
+    currentSubMoveSetIndex,
+    lastSubMoveSetIndex,
+    secondToLastSubMoveSetIndex,
+  ].every((value) => groups.includes(value));
+}
+
+function pickMoveFromSubMoveSet(currentSubMoveSet: SubMoveSet) {
+  // once we have chosen a different sub move set than the previous one,
+  // we want to pick a random move in this set to add it to the scramble
   generateNewSeed();
+  return currentSubMoveSet[lastGeneratedSeed.value % currentSubMoveSet.length];
 }
 
 function stringifiedScramble(scramble: Scramble): DisplayScramble {
@@ -62,6 +160,28 @@ function stringifiedScramble(scramble: Scramble): DisplayScramble {
 function lastNScrambles(n: number) {
   const nScrambles = scramblesHistory.value.slice(-n);
   return nScrambles.reverse();
+}
+
+function testScramblesRandomness() {
+  const repartition = scramblesHistory.value.reduce((acc, scramble) => {
+    scramble.forEach((move) => {
+      if (acc[move]) {
+        acc[move]++;
+      } else {
+        acc[move] = 1;
+      }
+    });
+    return acc;
+  }, {} as Record<string, number>);
+
+  // transform each move count into a percentage
+  const totalMoves = scramblesHistory.value.length * numberOfMoves.value;
+  const repartitionPercentage = Object.fromEntries(
+    Object.entries(repartition).map(([move, count]) => {
+      return [move, { percentage: (count / totalMoves) * 100, count }];
+    })
+  );
+  return repartitionPercentage;
 }
 
 export default {
@@ -74,4 +194,5 @@ export default {
   goToNextScramble,
   scramblesHistory,
   lastNScrambles,
+  testScramblesRandomness,
 };
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import SettingsThemes from "@/components/Settings/Themes.vue";
 import SettingsToggles from "@/components/Settings/Toggles.vue";
+import { useScramble } from "@/composables/scramble";
+const { testScramblesRandomness } = useScramble();
+
+function test() {
+  console.log(testScramblesRandomness());
+}
 </script>
 
 <template>
   <div class="flex flex-col gap-y-2 h-48 text-lg">
     <settings-themes />
     <settings-toggles />
+    <!-- <button @click="test">test</button> -->
   </div>
 </template>
